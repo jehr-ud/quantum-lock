9fee631cb0aa40f692da4bba9ffec73c3d830a54
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2026-09-21 11:18:02 -0500

    🌱 modify the schedule

--- src/app/core/seeds/seed.data.ts
+++ src/app/core/seeds/seed.data.ts
@@ -9,8 +9,8 @@ export const SEED = {
       code: '1542',
       name: 'Programación por Componentes',
       schedule: [
-        { day: 1, start: '07:00', end: '09:00' },
-        { day: 3, start: '09:00', end: '11:00' }
+        { day: 1, start: '12:00', end: '14:00' },
+        { day: 3, start: '14:00', end: '16:00' }
       ]
     },
 
@@ -19,8 +19,8 @@ export const SEED = {
       code: '1542',
       name: 'Programación por Componentes',
       schedule: [
-        { day: 2, start: '07:00', end: '09:00' },
-        { day: 4, start: '09:00', end: '11:00' }
+        { day: 1, start: '08:00', end: '10:15' },
+        { day: 3, start: '08:00', end: '10:15'  }
       ]
     },
 
@@ -29,8 +29,8 @@ export const SEED = {
       code: '1532',
       name: 'Inteligencia Artificial',
       schedule: [
-        { day: 1, start: '08:00', end: '10:00' },
-        { day: 5, start: '10:00', end: '12:00' }
+        { day: 1, start: '14:00', end: '16:00' },
+        { day: 3, start: '10:00', end: '12:00' }
       ]
     }
 
